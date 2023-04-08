6cbd6c683b8f8673c3f5de019e642ff97148e392
Tam <hi@tam.sx>
2023-04-08 14:39:56 +0100

    Thruster

--- src/player.rs
+++ src/player.rs
@@ -7,6 +7,9 @@ pub struct Player {
 	turn_speed : f32,
 }
 
+#[derive(Component)]
+pub struct PlayerThruster;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
@@ -33,11 +36,29 @@ fn setup (
 			move_speed: 500.,
 			turn_speed: 360f32.to_radians(),
 		},
-	));
+	)).with_children(|commands| {
+		commands.spawn((
+			SpriteSheetBundle {
+				texture_atlas: tilesheet.0.clone(),
+				sprite: TextureAtlasSprite {
+					index: 47,
+					color: Color::WHITE.with_a(0.75),
+					..default()
+				},
+				visibility: Visibility::Hidden,
+				transform: Transform::from_scale(Vec3::new(0.4, 0.5, 1.))
+					.with_translation(Vec3::new(0., -50., 0.)),
+				..default()
+			},
+			PlayerThruster,
+		));
+	});
 }
 
+// TODO: Aside from the keyboard input, the movement & rotation could be made generic (incl. showing the thrusters)
 fn player_movement(
 	mut query: Query<(&Player, &mut Transform)>,
+	mut thruster_query : Query<&mut Visibility, With<PlayerThruster>>,
 	time: Res<Time>,
 	input: Res<Input<KeyCode>>,
 	mut rotation_factor : Local<f32>,
@@ -51,7 +72,12 @@ fn player_movement(
 	if input.pressed(KeyCode::D) { *rotation_factor = -1.; }
 	
 	// Thrust
-	if input.pressed(KeyCode::W) { *movement_factor =  1.; }
+	if input.pressed(KeyCode::W) {
+		*movement_factor =  1.;
+		*thruster_query.single_mut() = Visibility::Visible;
+	} else {
+		*thruster_query.single_mut() = Visibility::Hidden;
+	}
 	
 	// Transform
 	t.rotate_z(*rotation_factor * player.turn_speed * time.delta_seconds());
@@ -62,4 +88,7 @@ fn player_movement(
 	// Decay
 	*rotation_factor *= 0.4;
 	*movement_factor *= 0.9;
+	
+	if *movement_factor < 0.001 { *movement_factor = 0. }
+	if *rotation_factor < 0.001 { *rotation_factor = 0. }
 }
